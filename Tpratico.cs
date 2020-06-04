08b472b38949b4e6ed6c0992e66bbbda5ecc6516
wagnermoretti <wjmt@estudante.ufla.br>
2020-06-04 12:05:56 -0300

    Criacao da classe C

--- Tpratico.cs
+++ Tpratico.cs
@@ -1,5 +1,13 @@
-public class A{
-    int A1;
-    float A2;
+using System;
 
+public class A{
+    public int A1 {get ; set};
+    public float A2 {get ; set};
+
+    public void MA1(){return Console.WriteLine(sf.GetMethod());}
+    public void MA2(){return Console.WriteLine(sf.GetMethod());}
+    public void MA3(){return Console.WriteLine("Alteração a classe A partir do clone")}
+
+    
 }
+
---
+++ classC.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class C{
+    public int C1 {get ; set};
+    public float C2 {get ; set};
+
+    public void MC1(){return Console.WriteLine(sf.GetMethod());}
+    public void MC2(){return Console.WriteLine(sf.GetMethod());}
+    }
