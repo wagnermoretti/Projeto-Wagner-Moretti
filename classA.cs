c312e7b65f42fbc15b5a942a7a3c2b13ba81e46c
wagnermoretti <wjmt@estudante.ufla.br>
2020-06-09 19:48:32 -0300

    subindo A

--- classA.cs
+++ classA.cs
@@ -1,8 +1,9 @@
-using System;
+﻿using System;
 
 public class A{
     public int A1 {get ; set};
     public float A2 {get ; set};
+    public string A3
 
     public void MA1(){return Console.WriteLine(sf.GetMethod());}
     public void MA2(){return Console.WriteLine(sf.GetMethod());}
